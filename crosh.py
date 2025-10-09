7efce4c688d5a62379ab9d1f8fc8ec45cf13f4a0
rmishra01-ui <rmishra01@founderstxstudents.org>
2025-10-09 16:26:44 -0500

    >_
    with >_

---
+++ crosh.py
@@ -0,0 +1,4 @@
+# 0tmu9843ut9n9t2t-4-t-ct--24--4t-v3-t--45-24yv-y-2-4yv-vy-452y-y----v2-y-2-4y5-c-2yvy-y--4-5-2-y-5--2---v-5--2-5-y-v-5-2--ct22-3---4t-2-t-v-5--44---4-v-24---y245-vy---43-v2v85-y8--m13m48-8-58-8v2-m-842m8-m42-y5m82-8m28-52ym85-y8m28-82-5myv-28y5-28m8y-v-y2m58-824-8my5-8v
+print("Welcome to the terminal!")
+while True:
+  print(input(">>> "))
